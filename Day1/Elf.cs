9180bc98c7a8756f50486dff1e534b02540b4bdb
martin-mee <119165946+martin-mee@users.noreply.github.com>
2022-12-01 18:06:57 +0000

    Day1

---
+++ Day1/Elf.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    public class Elf
+    {
+        public List<int> Calories { get; set; } = new List<int>();
+        public int Total { get; set; }
+
+        public Elf() { }
+    }
+}
