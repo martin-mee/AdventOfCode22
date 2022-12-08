5aea077a9849f5e041629db5338c04b0f30837d3
martin-mee <119165946+martin-mee@users.noreply.github.com>
2022-12-08 17:49:10 +0000

    Day 8

---
+++ Day8/Tree.cs
@@ -0,0 +1,22 @@
+﻿namespace Day8
+{
+    internal class Tree
+    {
+        internal int height { get; set; }
+        internal bool visible { get; set; }
+        internal int northView { get; set; }
+        internal int southView { get; set; }
+        internal int westView { get; set; }
+        internal int eastView { get; set; }
+
+        internal Tree(int height)
+        {
+            this.height = height;
+        }
+
+        internal int GetViewRange()
+        {
+            return northView * southView * westView * eastView;
+        }
+    }
+}
